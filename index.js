1fe18fcc861e7f599059afb5881bedfe1bba60a7
zhangyoof <Admin123>
2019-09-12 14:45:29 +0800

    delete

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-// registerComponent接收方法
-AppRegistry.registerComponent(appName, ()=>App);
--- src/services/service.js
+++
@@ -1,7 +0,0 @@
-import request from './request'
-import appUrlConfig from './appPortalConfig'
-
-// 登录
-export function login(param) {
-    return request('post', appUrlConfig.login.userLogin,param , false)
-}
